66ad022e18ace77a4670890b9c94a20fea970103
Gergely Simonics <99gergely99@gmail.com>
2019-04-11 11:47:12 +0200

    added postgreSQL databases

---
+++ databases/api_wars_dtb.sql
@@ -0,0 +1,32 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.5.6
+-- Dumped by pg_dump version 9.5.6
+
+ALTER TABLE IF EXISTS users DROP CONSTRAINT IF EXISTS pk_users_id CASCADE;
+ALTER TABLE IF EXISTS planet_votes DROP CONSTRAINT IF EXISTS pk_planet_votes_id CASCADE;
+ALTER TABLE IF EXISTS planet_votes DROP CONSTRAINT IF EXISTS fk_user_id CASCADE;
+
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id serial NOT NULL PRIMARY KEY,
+    user_name text UNIQUE,	
+    password text
+);
+
+DROP TABLE IF EXISTS planet_votes;
+CREATE TABLE planet_votes (
+	id serial NOT NULL PRIMARY KEY,
+	planet_id integer,
+	planet_name varchar,
+	user_id integer,
+	submission_time timestamp without time zone
+);
+
+
+ALTER TABLE planet_votes
+	ADD FOREIGN KEY (user_id) REFERENCES users(id);
+
