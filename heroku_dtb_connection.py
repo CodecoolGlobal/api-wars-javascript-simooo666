d69ad48d9add60d6174b18c1d3fddc05135ffe2d
Gergely Simonics <99gergely99@gmail.com>
2019-04-28 15:52:37 +0200

    trying to fix connection

--- heroku_dtb_connection.py
+++ heroku_dtb_connection.py
@@ -2,12 +2,15 @@ import os
 import psycopg2
 import urllib
 
-urllib.parse.uses_netloc.append('postgres')
-url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
-connection = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
-)
+
+def set_up_connection():
+    urllib.parse.uses_netloc.append('postgres')
+    url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
+    connection = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+    return connection
